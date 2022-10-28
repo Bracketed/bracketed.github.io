f26c82e82d5625ff912a4c732b1b0c034686c71f
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2022-10-28 09:34:18 +0100

    devin it up yk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function sayHelloWorld(): void {
+"use strict";
+function sayHelloWorld() {
     console.log("\n");
     console.log("*** Hello World from Type Script code. ***");
     console.log("\n");
